bf96fe1a5d7eaab573aa0788d14083cfaaacf0b6
Erandhi Madhushika <madhushikabae.20@uom.lk>
2026-01-10 21:36:30 +0530

    Create expense repository

--- src/main/java/com/example/expense_splitter/repository/ExpenseRepository.java
+++ src/main/java/com/example/expense_splitter/repository/ExpenseRepository.java
@@ -1,5 +1,9 @@
 package com.example.expense_splitter.repository;
 
-public class ExpenseRepository {
-    
+import com.example.expense_splitter.model.Expense;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.List;
+
+public interface ExpenseRepository extends MongoRepository<Expense, String> {
+    List<Expense> findByGroupId(String groupId);
 }
